47cfe44e0e70d4455405d0cfcc1256eb0ecbe464
Ana3Lu <anaquiva@unisabana.edu.co>
2025-03-08 08:37:29 -0500

    Añade Controller de Pago

---
+++ src/main/java/co/edu/unisabana/pago/PagoController.java
@@ -0,0 +1,41 @@
+package co.edu.unisabana.pago;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+@RequestMapping("/pago")
+public class PagoController {
+
+    private final WebClient webClient;
+
+    public PagoController(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    @CircuitBreaker(name = "notificacionCircuitBreaker", fallbackMethod = "fallbackNotificacion")
+    @PostMapping("/notificar")
+    public ResponseEntity<String> enviarNotificacion(@RequestParam String proveedor) {
+        String url = proveedor.equalsIgnoreCase("aldeamo")
+                ? "http://localhost:8081/notificación"
+                : "http://localhost:8082/notificación";
+
+        String response = webClient.post()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return ResponseEntity.ok("Notificación enviada: " + response);
+    }
+
+    public ResponseEntity<String> fallbackNotificacion(String proveedor, String mensaje, Throwable t) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("Fallo el servicio " + proveedor + ". Intentar más tarde.");
+    }
+}
+
